4306aa23ad6ab81acc7f1687d167baed95fe3311
Tim <timothy.yi7@gmail.com>
2017-07-18 14:54:47 -0700

    added styling

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Guess from './guess';
 import './app.css';
-
+import trooper from './imgs/trooper.jpg';
 
 const App = () => (
-    <div className="container">
-        <h1>Guessing Game</h1>
-        <h4>Guess a number 1-10</h4>
+    <div className="main-compartment">
+        <div className="trooperPic">
+            <img src={trooper} className="trooper"/>
+        </div>
+        <div className="game-title">
+            <h1 className="game_title">Guessing Game!</h1>
+        </div>
+        <div className="game-subtitle">
+            <h4 className="game_subtitle">Guess a number 1-10!</h4>
+        </div>
         <Guess />
     </div>
 );
--- src/components/guess.js
+++ src/components/guess.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-
+import './app.css';
 
 class Guess extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.state = {
@@ -13,21 +13,21 @@ class Guess extends Component {
         }
     }
 
-    handleChange(event){
-        this.setState({ userGuess: event.target.value });
+    handleChange(event) {
+        this.setState({userGuess: event.target.value});
     }
 
-    submitGuess(event){
+    submitGuess(event) {
         event.preventDefault();
-        let { randomNumber, userGuess } = this.state;
+        let {randomNumber, userGuess} = this.state;
         userGuess = parseInt(userGuess);
         let newOutcome = '';
 
-        if (userGuess > randomNumber){
+        if (userGuess > randomNumber) {
             newOutcome = "Too High";
-        } else if(userGuess < randomNumber){
+        } else if (userGuess < randomNumber) {
             newOutcome = "Too Low";
-        } else if(userGuess === randomNumber){
+        } else if (userGuess === randomNumber) {
             newOutcome = "You Got It";
         } else {
             newOutcome = "No Guess Given";
@@ -37,37 +37,53 @@ class Guess extends Component {
 
         this.setState({
             outcome: newOutcome,
-            history: [ ...this.state.history, newHistory ]
+            history: [...this.state.history, newHistory]
         });
     }
 
-    render(){
-        const { outcome, history } = this.state;
+    resetGuess(){
+        this.setState({
+            randomNumber: Math.floor(Math.random() * 10) + 1,
+            userGuess: '',
+            outcome: '',
+            history: []
+        })
+    }
+
+    render() {
+        const {outcome, history} = this.state;
 
         const historyOutput = history.map((item, index) => {
-           return <h5 key={index}>{item}</h5>;
+            return <h5 key={index}>{item}</h5>;
 
         });
 
-        return(
-            <div>
-                <div className="row">
-                    <div className="col-sm-6">
-                        <form onSubmit={(event) => this.submitGuess(event)}>
-                            <div className="input-group">
-                                <input onChange={(event) => this.handleChange(event)} type="number" className="form-control" value={this.state.userGuess} />
-                                <span className="input-group-btn">
-                                    <button className="btn btn-outline-danger">Make Guess</button>
-                                </span>
-                            </div>
-                        </form>
-                        <h3 className={( (outcome === "Too Low") ? 'text-primary' : (outcome === "Too High") ? 'text-danger' : 'text-success' )}>{this.state.outcome}</h3>
-                    </div>
-                    <div className="col-sm-6">
-                        {historyOutput}
+        return (
+            <div className="total-area">
+                <div className="game-area">
+                    <div className="text-box">
+                        <div className="guess-box">
+                            <form onSubmit={(event) => this.submitGuess(event)}>
+                                <div className="input-group">
+                                    <input onChange={(event) => this.handleChange(event)} type="number"
+                                       className="form-control" value={this.state.userGuess}/>
+                                    <span className="input-group-btn">
+                                        <button className="btn btn-primary">Make Guess</button>
+                                    </span>
+                                </div>
+                            </form>
+                            <h3 className={( (outcome === "Too Low") ? 'text-primary' : (outcome === "Too High") ? 'text-danger' : 'text-success' )}>{this.state.outcome}</h3>
+                        </div>
                     </div>
                 </div>
+
+                <div className="history pre-scrollable col-sm-6">
+                    {historyOutput}
+                </div>
+                <button onClick={(event) => this.resetGuess(event)} className="reset_button btn btn-danger">Reset</button>
             </div>
+
+
         )
     }
 }
